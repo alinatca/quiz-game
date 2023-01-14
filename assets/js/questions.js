e4376e77ac2c62ea169fa47878742a73d09a347f
unknown <alina_tca@yahoo.com>
2023-01-14 10:07:53 +0000

    Creating the question file and function

---
+++ assets/js/questions.js
@@ -0,0 +1,19 @@
+
+var questions = [
+
+    {title:"Inside which HTML element do we put the Javascript?",
+    choices: ["js tag","javascript tag", "script tag","scrpting tag"],
+    answer:"script tag"
+    },
+    
+    {title:"The External JavaScript file must contain the <script> tag",
+    choices: ["True", "False"],
+    answer:"False"
+    },
+    
+    {title:"JavaScript is a ___ -side programming language.",
+    choices: ["Client", "Server", "Both", "None"],
+    answer:"Both"
+    }
+    
+    ];
